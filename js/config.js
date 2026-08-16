4e9782030f44d6c0125b24c2d34b55ee9532b0cf
tccgproject6-lab <tccgproject6@gmail.com>
2026-08-16 06:37:01 +0300

    Connect Supabase production configuration

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
-window.HUB_CONFIG={supabaseUrl:"YOUR_SUPABASE_URL",supabaseAnonKey:"YOUR_SUPABASE_ANON_KEY"};
+window.HUB_CONFIG={
+    supabaseUrl:"https://bysheihhqvvyhjmfwnee.supabase.co",
+    supabaseAnonKey:"sb_publishable_tR8BZ8Q4qII_ajZzlhm-qQ_EE85ARE1"
+};
+
+window.HUB_ICE_SERVERS=[
+    {
+        urls:["stun:stun.l.google.com:19302"]
+    }
+];
